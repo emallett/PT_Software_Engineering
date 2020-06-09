902d2d81ad8c0da15f85e0e2e2512c65754e1b14
Beth Mallett <bethmallett614@gmail.com>
2020-06-09 15:39:54 -0400

    copied from incorrect file

---
+++ Mod1/week-2/w2d2/answers.js
@@ -0,0 +1,173 @@
+ //Part 1
+ HTML -Tags
+ HTML -elements
+ CSS -Intro
+ CSS -Basic 
+ 
+ 
+ 
+ 
+ ////////////////////////////////
+ // Easy Going loop from 1- 20
+ ////////////////////////////////
+ for (var i =0; i < 21; ++i) 
+     console.log(i);
+ 
+ ////////////////////////////////
+ // Get Even
+ ////////////////////////////////
+ for (var i =0; i < 201; ++i) {ifi%2}
+     console.log(i);
+ 
+ 
+ ////////////////////////////////
+ // Fizz Buzz
+ ////////////////////////////////
+ 
+ //string
+     let str1= "Fizz"
+     let str2 ="Buzz"
+     let str3 ="fizzBuzz"
+ 
+ 
+ for (var counter = 0; counter <= 101; counter++) 
+ 
+      for(let i = 1; i < 101; i++){ 
+          if (i % 3 == 0)
+          console.log (str3)
+          else if (i % 5 == 0)
+          console.log  (str2)
+          
+          else if (i % 5 === 0 && i % 3 == 0)
+                 console.log(str3)}
+ 
+ ////////////////////////////////
+ // Wild Wild Life
+ 
+ const wolfy = ["Wolfy", "wolf", 16, "Yukon Territory"]
+ const sharky = ["Sharky", "shark", 20, "Left Coast"]
+ const plantee = ["Plantee", "plant",  5000 , "Mordor"]
+ const porgee = ["Porgee", "Porg", 186, "Ahch-To"]
+ const dart = ["D'Art" , "Demogorgan Dog", 2, "Upside Down"]
+ 
+ //1
+ plantee[2] = 5001;
+ console.log(plantee);
+ 
+ //2
+ wolfy[3] = 'Gotham City'
+ console.log(wolfy);
+ 
+ //3 
+ dart[4] = 'Hawkins'
+ console.log(dart);
+ 
+ //4
+ wolfy[0] = 'Gameboy'
+ console.log(wolfy);
+ 
+ ////////////////////////////////
+ 
+ 
+ ////////////////////////////////
+ // Yell at the Ninja Turtles
+ ////////////////////////////////
+ const ninjaTurtles = ('Donatello', 'Leonardo', 'Raphael', 'Michaelango')
+     for (let yellAtTurtles =name.toUpperCase();
+         console.log(toUpperCase(ninjaTurtles[i]));
+     
+ 
+ ////////////////////////////////
+ // Methods, Revisited
+ ////////////////////////////////
+ 
+ favMovies = ['Jaws', 'The Fellowship of the Ring', 'Howl\'s Moving Castle', 'Django Unchained', 'Cloud Atlas', 'The Usual Suspects', 'Toy Story', 'Conan the Barbarian', 'Titanic', 'Harry Potter', 'Fried Green Tomatoes', 'Volver', 'Oculus', 'Seven', 'Black Panther', 'Harry Potter', 'Imitation of Life', 'Snatch', 'Fast and Furious'];
+ 
+ console.log(favMovies.indexOf ('Titanic')
+ 
+ //1
+  (favMovies.sort()
+ 
+ //2    
+  (favMovies.pop()
+ 
+  //3
+  (favMovies.push('Guardian of the Galaxy')
+ 
+  //4
+  (favMovies.reverse())
+ 
+  //5
+  (favMovies.shift(remove,'Fast and the Furious')
+ 
+  //6
+  (favMovies.unshift()
+     (console.log(favMovies.unshift))
+ 
+  //7
+  (favMovies.splice(favMovies[3], add[19]='Avatar')
+ 
+  //8
+  (favMovies.slice(9)
+     (console.log(favMovies.slice(9)))
+ 
+  //9
+ 
+ (favMovies.splice)
+     (console.log(favMovies))
+     /* no idea what is going on here */
+ 
+ //10
+ (Console.log(favMovies.slice))
+ 
+ 
+ ////////////////////////////////
+ // Where is Waldo
+ ////////////////////////////////
+ const whereIsWaldo = [["Timmy", "Frank"], "Eggbert", ["Lucinda", "Jacc", "Neff", "Snoop"],
+  ["Petunia", ["Baked Goods", "Waldo"]]];
+ 
+  removeEggbert
+  whereIsWaldo.splice (1,1);
+   (console.log(whereIsWaldo))
+     
+  whereIsWaldo[5] = 'No One'
+     (console.log(whereIsWaldo))
+ 
+ 
+ ////////////////////////////////
+ //  Excited Kitten
+ ////////////////////////////////
+ 
+ //1 
+ let meow ='Love me, pet me! HSSSSSS!'; 
+ for (let i=0; i<21 ; i++)
+ 
+ //2
+ //For every number in your loop, log 
+ 
+ var kittyTalk=['human','why you taking pictures of me?','the catnip made me do it','why does the red dot always get away']
+ 
+ let randomValue = kittyTalk
+ Math.ceil()
+ Math.round ()
+  Math.floor()  //// no idea
+ 
+ 
+ ////////////////////////////////
+ //  Find the Median
+ ////////////////////////////////
+ 
+ const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
+ Arrays.sort(14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12);
+ if   
+ 
+ ///median num=
+ //Expected output:(function=15)
+ 
+ //Math.floor(//'something')
+  
+ (18 % 2 != 0)
+ (double)[18/2]
+     // check for even case 
+        (double)(a[(n - 1) / 2] + a[n / 2]) / 2.0; //I got this equation online
